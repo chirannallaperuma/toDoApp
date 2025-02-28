95a050e6a1da2d50026343482a7d2186e563c6ed
chiran nallaperuma <chiran@i4tlabs.io>
2025-02-28 08:15:44 +0530

    docker setup done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import Test from './components/Test.vue';
+
+const app = createApp({});
+app.component('test-component', Test);
+app.mount('#app');
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tasks', 'App\Http\Controllers\TaskController@index');
+Route::post('/tasks', 'App\Http\Controllers\TaskController@store');
+Route::put('/tasks/{id}', 'App\Http\Controllers\TaskController@update');
